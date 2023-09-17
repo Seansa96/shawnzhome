717bcf0418a6eb61bd3916bfc235b0bce837a534
Seansa96 <seansattaur90@hotmail.com>
2023-09-17 14:38:21 -0400

    Stylized footer, added responsiveness to navlink, added twitch embed

---
+++ script.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.maxHeight === "0px") {
+      x.style.maxHeight = "400px";
+    } 
+    else  {
+      x.style.maxHeight = "0px";
+    }
+};
+
