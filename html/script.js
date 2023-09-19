2b7eded01818c83c1161972cac1cac487b1d4caa
Seansa96 <seansattaur90@hotmail.com>
2023-09-18 23:32:22 -0400

    Fixed script.js

--- html/script.js
+++ html/script.js
@@ -1,12 +1,25 @@
 function myFunction() {
-    var x = document.getElementById("myLinks");
-    var currentHeight = window.getComputedStyle(x).getPropertyValue("max-height"); /* Get the element's current height since we don't know it yet */
+  var x = document.getElementById("myLinks");
+  var currentHeight = window.getComputedStyle(x).getPropertyValue("max-height"); /* Get the element's current height since we don't know it yet */
 
-    if (currentHeight === "0px") {
-      x.style.maxHeight = "400px";
-    } 
-    else  {
-      x.style.maxHeight = "0px";
-    }
+  if (currentHeight === "0px") {
+    x.style.maxHeight = "400px";
+  } 
+  else  {
+    x.style.maxHeight = "0px";
+  }
 };
 
+function search() {
+const searchTerm = document.getElementById('searchInput').value;
+
+fetch(`/api/search?q=${searchTerm}`)
+  .then(response => response.json())
+  .then(data => {
+    // Handle and display data as you wish.
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+}
